2f3af9b8b02e11db19aa9e6d342076dffa22783d
Alessandro Giunta <alegiunt99@gmail.com>
2022-03-22 17:20:08 +0100

    Creo 10 oggetti (zucchine)

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -2,4 +2,96 @@ console.log('JS OK!')
 
 /*Crea 10 oggetti che rappresentano una zucchina.
 Dividi in due array separati le zucchine che misurano meno o più di 15cm.
-Infine stampa separatamente quanto pesano i due gruppi di zucchine. */ 
+Infine stampa separatamente quanto pesano i due gruppi di zucchine. */
+
+
+// creo 10 oggetti come zucchine 
+const zucchNera = {
+    varietà: 'nera',
+
+    peso: 45, // grammi
+
+    lunghezza: 15 // cm
+};
+
+// Zucchina romanesca
+const zucchRoma = {
+    varietà: 'romanesca',
+
+    peso: 60, // grammi
+
+    lunghezza: 25 // cm
+}; 
+
+// Zucchina fiorentina
+const zucchFiore = {
+    varietà: 'fiorentina',
+
+    peso: 80, // grammi
+
+    lunghezza: 13 // cm
+};
+ 
+// Zucchina napoletana
+const zucchNapoli = {
+    varietà: 'napoletana',
+
+    peso: 75, // grammi
+
+    lunghezza: 22  // cm
+};
+ 
+// Zucchine tonde
+const zucchTonda = {
+    varietà: 'tonda',
+
+    peso: 30, // grammi
+
+    lunghezza: 11  // cm
+};
+ 
+// Zucchine trombetta
+const zucchTromb = {
+    varietà: 'trombetta',
+
+    peso: 90, // grammi
+
+    lunghezza: 24  // cm
+};
+ 
+// Zucchina patisson
+const zucchPatis = {
+    varietà: 'patisson',
+
+    peso: 35, // grammi
+
+    lunghezza: 17  // cm
+};
+ 
+// Zucchina gialla 
+const zucchGialla = {
+    varietà: 'gialla',
+
+    peso: 85, // grammi
+
+    lunghezza: 12 // cm
+};
+ 
+// Zucchina rugosa friulana
+const zucchFriul = {
+    varietà: 'rugosa friulana',
+
+    peso: 55, // grammi
+
+    lunghezza: 14  // cm
+};
+ 
+// Zucchina Crookneck
+const zucchCrook = {
+    varietà: 'Crookneck',
+
+    peso: 95, // grammi
+
+    lunghezza: 19  // cm
+};
+
