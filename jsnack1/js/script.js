1247c4c3dda8d75a4ab24dbf29e6917bb5ce34cd
Alessandro Giunta <alegiunt99@gmail.com>
2022-03-22 16:08:41 +0100

    Init

---
+++ jsnack1/js/script.js
@@ -0,0 +1,7 @@
+console.log('JS OK!')
+
+/* Crea un array di 10 oggetti che rappresentano una zucchina,
+ indicando per ognuna varietà, peso e lunghezza. 
+ Calcola quanto pesano tutte le zucchine.*/
+ 
+ 
---
+++ jsnack3/js/script.js
@@ -0,0 +1,3 @@
+console.log('JS OK!')
+
+/*Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC) */ 
