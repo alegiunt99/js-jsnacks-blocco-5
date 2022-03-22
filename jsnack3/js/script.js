2b878d2bc3b6bb4a63555d47236ef9934e4419ba
Alessandro Giunta <alegiunt99@gmail.com>
2022-03-22 18:59:16 +0100

    Aggiungo la funzione che inverte la parola... ...inserita col prompt

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,3 +1,17 @@
 console.log('JS OK!')
 
-/*Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC) */ 
+/*Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC) */
+
+const stringaGenerata = prompt('scrivi una parola');
+
+const parolaGirata = reverseString(stringaGenerata);
+
+console.log(parolaGirata)
+
+
+function reverseString(stringa) {
+
+
+    return stringa.split("").reverse().join("");
+
+}
